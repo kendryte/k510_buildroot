eb4fb26d2380d8690ab626e30ef19f74dfc92cd0
Wentao Wu <wuwentao2000@126.com>
2022-05-08 13:25:47 +0800

    update nncase to v1.6 and sync dev to main (#5)
    * add github actions (#2)
    
    * modify lic (#3)
    
    * nncase update to 1.6 (#4)
    
    Co-authored-by: HZ1 <huangziyi010@live.cn>
    Co-authored-by: wangjianxin-canaan <102512117+wangjianxin-canaan@users.noreply.github.com>

--- package/nncase_linux_runtime/nncase_linux_runtime.mk
+++ package/nncase_linux_runtime/nncase_linux_runtime.mk
@@ -5,13 +5,14 @@
 ################################################################################
 #https://github.com/kendryte/nncase/releases/download/v1.4.0/nncase-1.4.0.20220303-cp310-cp310-manylinux_2_24_x86_64.whl
 
-#https://ai.b-bug.org/~wangjianxin/sdk_thin/nncase_v1.4.0.tgz
-NNCASE_PIP_VD = 1.5.0.20220331
-NNCASE_PIP_K510_VD = 1.5.0.20220401
-NNCASE_VSTR = v1.5.0
+#https://github.com/kendryte/nncase/releases/download/v1.6.0/nncase-k510-1.6.0.tgz
+#https://github.com/kendryte/nncase/releases/download/v1.6.0/nncase-k510-1.6.0.tgz
+NNCASE_PIP_VD = 1.6.0.20220505
+NNCASE_PIP_K510_VD = 1.6.0.20220505
+NNCASE_VSTR = v1.6.0
 
-NNCASE_LINUX_RUNTIME_SOURCE = nncase_$(NNCASE_VSTR).tgz
-NNCASE_LINUX_RUNTIME_SITE = https://ai.b-bug.org/~wangjianxin/sdk_thin
+NNCASE_LINUX_RUNTIME_SOURCE = nncase-k510-1.6.0.tgz
+NNCASE_LINUX_RUNTIME_SITE = https://github.com/kendryte/nncase/releases/download/$(NNCASE_VSTR)
 NNCASE_LINUX_RUNTIME_EXTRA_DOWNLOADS = https://github.com/kendryte/nncase/releases/download/$(NNCASE_VSTR)/nncase-$(NNCASE_PIP_VD)-cp310-cp310-manylinux_2_24_x86_64.whl 
 NNCASE_LINUX_RUNTIME_EXTRA_DOWNLOADS += https://github.com/kendryte/nncase/releases/download/$(NNCASE_VSTR)/nncase-$(NNCASE_PIP_VD)-cp36-cp36m-manylinux_2_24_x86_64.whl
 NNCASE_LINUX_RUNTIME_EXTRA_DOWNLOADS += https://github.com/kendryte/nncase/releases/download/$(NNCASE_VSTR)/nncase-$(NNCASE_PIP_VD)-cp37-cp37m-manylinux_2_24_x86_64.whl
@@ -37,8 +38,11 @@ NNCASE_LINUX_RUNTIME_INSTALL_TARGET = NO
 
 define NNCASE_LINUX_RUNTIME_INSTALL_STAGING_CMDS
 	$(call NNCASE_LINUX_RUNTIME_nncase_nncase_k510_pip3_install)
-	cp -rf $(@D)/riscv64/include/* $(STAGING_DIR)/usr/include/
-	cp -rf $(@D)/riscv64/lib/*.a $(STAGING_DIR)/usr/lib/
+	cp -rf $(@D)/riscv64/gsl $(STAGING_DIR)/usr/include/
+	cp -rf $(@D)/riscv64/gsl-lite  $(STAGING_DIR)/usr/include/
+	cp -rf $(@D)/riscv64/mpark  $(STAGING_DIR)/usr/include/
+	cp -rf $(@D)/riscv64/nncase/include/* $(STAGING_DIR)/usr/include/
+	cp -rf $(@D)/riscv64/nncase/lib/*.a $(STAGING_DIR)/usr/lib/
 endef
 
 $(eval $(generic-package))
