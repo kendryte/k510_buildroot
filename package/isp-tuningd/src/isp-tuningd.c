fd352c682c9b0d311b1ef5cbb2fba7901b569cb1
HZ1 <huangziyi010@live.cn>
2022-05-19 19:19:36 +0800

    Update isp-tuningd.c

--- package/isp-tuningd/src/isp-tuningd.c
+++ package/isp-tuningd/src/isp-tuningd.c
@@ -1,6 +1,4 @@
 /* Copyright (c) 2022, Canaan Bright Sight Co., Ltd
- * 
- * @author: huangziyi<huangziyi@canaan-creative.com> 
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
