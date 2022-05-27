e0fb15a40d470477f0cee9a044e9275b77875c30
gh-sxp <100889366+gh-sxp@users.noreply.github.com>
2022-05-27 10:14:58 +0800

    Add coding functions: Deblocking，slice, entropy coding, SEI (#67)
    * Add coding parameters and advanced functions
    
    * tcperror
    
    * Network transmission data error
    
    * Modify the network card driver cache
    
    * tcp error
    
    * tcp error
    
    * tcp error
    
    * add function: ffmpeg push RTMP stream
    
    * Modify ffmpeg compilation parameters: remove enable alsa
    
    * Add configuration items of RTMP package
    
    * Add ffmpeg dependency configuration
    
    * rebuild again
    
    * Modify rtmpdump download address
    
    * Modify rtmpdump download address
    
    * Modify librtmp package hash verification
    
    * modify librtmp hash
    
    * modify librtmp hash
    
    * modify librtmp hash
    
    * modify librtmp hash
    
    * Modify librtmp decompression name
    
    * modify librtmp config
    
    * modify librtmp patch
    
    * remove buildroot-2020.02.11 cache dir
    
    * Modify ffmpeg K510 encode: initialization encoding configuration
    
    * Initialize minqp and maxqp and solve mx2-723, mx2-725 and mx2-727 bugs
    
    * V4l2 JEPG code, press Q to exit core dump
    
    * Add coding functions: filtering, slice, entropy coding, SEI
    
    * Remove the previous RTSP obsolete code
    
    Co-authored-by: Wentao Wu <wuwentao2000@126.com>

--- package/encode_app/src/main.cpp
+++ package/encode_app/src/main.cpp
@@ -864,20 +864,6 @@ static void endof_encode()
             pCtx->out_pic[i], (pCtx->out_pic[i] * 1000.0) / ((get_time()-pCtx->start_time[i])/1000000.0));
   
     printf("total_out_size %d\n", pCtx->total_out_size[i]);
-    if( pCtx->enable_rtsp[i] == 0){  
-      if(pCtx->total_out_size[i] > 0)
-      {
-        if(pCtx->total_out_size[i] <= pCtx->stream_size[i])
-        {
-          fwrite(pCtx->out_buffer[i], 1, pCtx->total_out_size[i], pCtx->out_file[i]);
-        }
-        else
-        {
-          printf("out_buffer is too small\n");
-          fwrite(pCtx->out_buffer[i], 1, pCtx->stream_size[i], pCtx->out_file[i]);
-        }
-      }  
-    }
 
     if(pCtx->out_file[i])
       fclose(pCtx->out_file[i]);
@@ -2186,6 +2172,12 @@ int main(int argc, char *argv[])
       if(!pCtx->Cfg[i].MinQP)             pCtx->Cfg[i].MinQP       = 0;//from 0 to SliceQP
       if(!pCtx->Cfg[i].MaxQP)             pCtx->Cfg[i].MaxQP       = 51;//from SliceQP to 51
       if(!pCtx->Cfg[i].roiCtrlMode)       pCtx->Cfg[i].roiCtrlMode = ROI_QP_TABLE_NONE;
+
+      pCtx->Cfg[i].encDblkCfg.disable_deblocking_filter_idc        = 0;
+      pCtx->Cfg[i].encDblkCfg.slice_beta_offset_div2               = 1;
+      pCtx->Cfg[i].encDblkCfg.slice_alpha_c0_offset_div2           = 1;
+      pCtx->Cfg[i].entropyMode                                     = ENTROPY_MODE_CAVLC;
+      pCtx->Cfg[i].sliceSplitCfg.bSplitEnable                      = false;
       
     }
 
@@ -2244,6 +2236,11 @@ int main(int argc, char *argv[])
         pCtx->Cfg[i].FrameRate = pCtx->out_framerate[i];
         printf("ch%d-Cfg.Framerate: %d\n", i, pCtx->Cfg[i].FrameRate);
         pCtx->hEnc[i] = VideoEncoder_Create(&pCtx->Cfg[i]);
+        if (NULL == pCtx->hEnc[i])
+        {
+          printf("[%s] VideoEncoder_Create failed\n",__FUNCTION__);
+          return -1;
+        }
 
         if (ROI_QP_TABLE_NONE != pCtx->Cfg[i].roiCtrlMode)
         {
