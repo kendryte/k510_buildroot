62eacd2df3c8ea46acd9ae518ac8defdf62a24ff
shiwentao <shiwentao@canaan-creative.com>
2022-06-22 19:19:59 +0800

    modify mk file

--- package/aws_iot_test/aws_iot_test.mk
+++ package/aws_iot_test/aws_iot_test.mk
@@ -4,9 +4,31 @@
 #
 ################################################################################
 AWS_LOCAL_PATH:= $(realpath $(dir $(lastword $(MAKEFILE_LIST))))
-AWS_IOT_TEST_SITE = $(AWS_LOCAL_PATH)/subscribe_publish_sample
-AWS_IOT_TEST_SITE_METHOD = local
+AWS_IOT_TEST_SITE = $(AWS_LOCAL_PATH)
+AWS_IOT_TEST_SITE_METHOD = file
+
 FILE=$(AWS_LOCAL_PATH)/subscribe_publish_sample
+
+AWS_IOT_TEST_DATA_SITE = https://github.com/Mbed-TLS/mbedtls/archive/refs/tags/mbedtls-2.11.0.tar.gz
+AWS_IOT_TEST_SOURCE = subscribe_publish_sample.tar
+
+MBEDTLS_DIR_S = $(AWS_LOCAL_PATH)/subscribe_publish_sample/external_libs/
+
+AWS_IOT_TEST_EXTRA_DOWNLOADS = $(call qstrip,$(AWS_IOT_TEST_DATA_SITE))
+
+
+AWS_IOT_TEST_PRE_DOWNLOAD_HOOKS = ai_iot_test_pre_download_hooks_tar_code
+define ai_iot_test_pre_download_hooks_tar_code
+	rm -rf $(AWS_LOCAL_PATH)/subscribe_publish_sample.tar;
+	tar -cf $(AWS_LOCAL_PATH)/subscribe_publish_sample.tar -C$(AWS_LOCAL_PATH) subscribe_publish_sample
+endef
+
+AWS_IOT_TEST_POST_EXTRACT_HOOKS = AWS_IOT_TEST_POST_EXTRACT_HOOKS_DATA
+
+define AWS_IOT_TEST_POST_EXTRACT_HOOKS_DATA
+	rm -rf $(AWS_LOCAL_PATH)/subscribe_publish_sample.tar;
+	tar -zvxf $(AWS_IOT_TEST_DL_DIR)/mbedtls-2.11.0.tar.gz -C $(MBEDTLS_DIR_S)
+endef
 
 
 define AWS_IOT_TEST_COMPILE_MODEL
