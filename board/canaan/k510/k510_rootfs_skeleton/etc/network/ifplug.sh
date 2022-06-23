730ccca3efa2059013e5955a5a2b2561e0513799
wangjianxin <wangjianxin@canaan-creative.com>
2022-06-23 16:02:41 +0800

    net down ,clear ip

--- board/canaan/k510/k510_rootfs_skeleton/etc/network/ifplug.sh
+++ board/canaan/k510/k510_rootfs_skeleton/etc/network/ifplug.sh
@@ -4,7 +4,9 @@
 if [ "$1" = "eth0" ]; then 
 	if [ "$2" = "up" ]; then 
 		ifdown  eth0;
-		ifup  eth0;		
+		ifup  eth0;	
+	else
+		ifconfig eth0 0.0.0.0
 	fi;
 fi
 #/usr/sbin/ifplugd   -n -p  -I -i eth0 -r /etc/network/ifplug.sh &
