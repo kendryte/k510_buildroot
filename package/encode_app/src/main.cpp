4bc37bfb7c37a5f75d728337936ea00d8b384861
gh-sxp <sunxiaopeng@canaan-creative.com>
2022-08-09 18:05:30 +0800

    modify maxqp

--- package/encode_app/src/main.cpp
+++ package/encode_app/src/main.cpp
@@ -2382,7 +2382,8 @@ int main(int argc, char *argv[])
           printf("slice qp error\n");
           return -1;
         }
-	pCtx->Cfg[i].MaxQP = 100;
+	if (pCtx->Cfg[i].SliceQP > pCtx->Cfg[i].MaxQP)
+	    pCtx->Cfg[i].MaxQP = pCtx->Cfg[i].SliceQP;
       }
       else
       {
